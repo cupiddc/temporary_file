ab5b0b205633123731a8b4fd58349306ef842335
Surbhi <surbhigupta70896@gmail.com>
2023-12-26 07:03:31 +0530

    Adding addition file

---
+++ addition.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int add(int a, int b){
+    int c=a+b;
+    return c;
+}
+int main(){
+ cout <<"additon of the two integer values "<<add(2,4)<<endl;
+ return 0;
+}
